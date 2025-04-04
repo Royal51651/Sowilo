acb9c279f10a6051d502887cd8294a40aa56e1a3
Royal51651 <georgeegges682@gmail.com>
2025-04-04 14:47:59 -0400

    :3

--- src/main.rs
+++ src/main.rs
@@ -39,9 +39,7 @@ fn calculate_saturation(red: u8, green: u8, blue: u8) -> f32 {
         min = normal_blue;
     }
 
-    println!("Max: {max} | Min: {min}");
-
-    if(max == min){
+    if max == min {
         return 0.0;
     }
 
@@ -306,6 +304,93 @@ fn generate_palette_combo(path: &str, hues: u32, luminosities: u32) -> Vec<Vec<i
     color_list
 }
 
+fn generate_palette_hue_and_sat(path: &str, hues: u32, saturations: u32) -> Vec<Vec<image::Rgba<u8>>> {
+    let img = image::open(path).unwrap();
+    let (imgx, imgy) = img.dimensions();
+    let number_of_pixels: u128 = imgx as u128 * imgy as u128;
+    // calculate the min hue and the max hue
+    let mut min_hue: f32 = 360.0;
+    let mut max_hue: f32 = 0.0;
+    for (_x, _y, pixel) in img.pixels() {
+        let hue = calculate_hue(pixel[0], pixel[1], pixel[2]);
+        if hue < min_hue {
+            min_hue = hue;
+        }
+        if hue > max_hue {
+            max_hue = hue;
+        }
+    }
+
+    let delta_hue = max_hue - min_hue;
+    let hue_slice_size: f32 = delta_hue / hues as f32;
+
+    // create the 2d vector with pixel data that will store the actual values later on
+    let mut hue_slices: Vec<Vec<image::Rgba<u8>>> = Vec::new();
+    for _i in 0..hues {
+        let lum_vector: Vec<image::Rgba<u8>> = Vec::new();
+        hue_slices.push(lum_vector);
+    }
+
+    // sort each pixel by it's hue
+    for (_x, _y, pixel) in img.pixels() {
+        let hue = calculate_hue(pixel[0], pixel[1], pixel[2]);
+        let index: usize = (hue / hue_slice_size) as usize;
+        if index == hues as usize {
+            hue_slices[0].push(pixel);
+        } else {
+            hue_slices[index].push(pixel);
+        }
+        
+    }
+
+    let mut color_list: Vec<Vec<image::Rgba<u8>>> = Vec::new();
+    let mut actual_colors: u128 = 0;
+    for i in hue_slices {
+        let mut min_sat: f32 = 360.0;
+        let mut max_sat: f32 = 0.0;
+        // calculate the maximum and minimum luminosities for the sub-lists
+        for pixel in &i {
+            let sat = calculate_saturation(pixel[0], pixel[1], pixel[2]);
+            if sat > max_sat {
+                max_sat = sat;
+            }
+            if sat < min_sat {
+                min_sat = sat;
+            }
+        }
+        let delta_sat = max_sat - min_sat;
+        let sat_slice_size = delta_sat / saturations as f32;
+        // loop through the pixels
+        let mut sub_list: Vec<image::Rgba<u8>> = Vec::new();
+        for j in 0..saturations {
+            let mut red_total: u128 = 0;
+            let mut green_total: u128 = 0;
+            let mut blue_total: u128 = 0;
+            let mut count: u128 = 0;
+            
+            // if they fit in a valid luminosity range, add them to the total;
+            for pixel in &i {
+                let sat = calculate_saturation(pixel[0], pixel[1], pixel[2]);
+                
+                if sat >= sat_slice_size * (j as f32) && sat <= sat_slice_size * (j as f32 + 1.0) {
+                    red_total += pixel[0] as u128;
+                    green_total += pixel[1] as u128;
+                    blue_total += pixel[2] as u128;
+                    count += 1;
+                }
+            }
+            if count != 0 {
+                sub_list.push(image::Rgba([(red_total / count) as u8, (green_total / count) as u8, (blue_total / count) as u8, 255]));        
+            } else {
+                sub_list.push(BLACK);
+            }
+            actual_colors += 1;
+        }
+        color_list.push(sub_list);
+    }
+    color_list
+}
+
 fn draw_pixel(x: u32, y: u32, color: [u8; 4], input: &mut Buf){
     let pixel = input.get_pixel_mut(x, y);
     *pixel = image::Rgba(color);
@@ -448,9 +533,9 @@ add a function to calculate using saturation in addition to hue and luminosity
 
 fn main() {
     let size: u32 = 8;
-    let path = String::from("image.jpg");
+    let path = String::from("starry_night.jpg");
     println!("Generating By Combo");
-    let colors = generate_palette_combo(&path,size, size);
+    let colors = generate_palette_hue_and_sat(&path,size, size);
     //println!("{}", calculate_saturation(0, 1, 1));
     format_output(colors, true);
     println!("Done");
